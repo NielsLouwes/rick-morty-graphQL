1dbd49fd05d95948f029f4737954518487f30a78
Niels Louwes <niels.louwes@and.digital>
2023-08-09 15:18:01 +0200

    added roboto texttheme to page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -8,101 +8,108 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: const Text('Characters'),
-        centerTitle: true,
+    return Theme(
+      data: Theme.of(context).copyWith(
+        textTheme: GoogleFonts.robotoTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
-      body: ref.watch(fetchCharactersProvider).maybeWhen(
-        fetching: () {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
-        fetched: (characters) {
-          return ListView(
-            children: characters
-                .map((character) => Card(
-                      color: const Color.fromARGB(255, 69, 68, 68),
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 10), // Adjust as needed
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // Image to the left
-                            Image.network(
-                              character.image!,
-                              height: 150,
-                              fit: BoxFit.cover,
-                              width: 150, // Adjust the width as needed
-                            ),
-                            // Adding a little spacing between image and text
-                            const SizedBox(width: 10),
-                            // Text content to the right of the image
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    character.name!,
-                                    style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 22,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(
-                                    '${character.status!} - ${character.species}',
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  const Text(
-                                    'Last known location:',
-                                    style: TextStyle(
-                                        color:
-                                            Color.fromARGB(255, 141, 147, 149)),
-                                  ),
-                                  Text(
-                                    character.location?.name ?? 'Unknown',
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  const Text(
-                                    'First seen in:',
-                                    style: TextStyle(
-                                        fontSize: 16,
-                                        color:
-                                            Color.fromARGB(255, 141, 147, 149)),
-                                  ),
-                                  Text(
-                                    character.episode?.isNotEmpty == true
-                                        ? character.episode![0].name ??
-                                            'Unknown'
-                                        : 'Unknown',
-                                    style: const TextStyle(
-                                        color: Colors.white, fontSize: 16),
-                                  )
-                                ],
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: const Text('Characters'),
+          centerTitle: true,
+        ),
+        body: ref.watch(fetchCharactersProvider).maybeWhen(
+          fetching: () {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+          fetched: (characters) {
+            return ListView(
+              children: characters
+                  .map((character) => Card(
+                        color: const Color.fromARGB(255, 69, 68, 68),
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10), // Adjust as needed
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // Image to the left
+                              Image.network(
+                                character.image!,
+                                height: 150,
+                                fit: BoxFit.cover,
+                                width: 150, // Adjust the width as needed
                               ),
-                            ),
-                          ],
+                              // Adding a little spacing between image and text
+                              const SizedBox(width: 10),
+                              // Text content to the right of the image
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      character.name!,
+                                      style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      '${character.status!} - ${character.species}',
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    const Text(
+                                      'Last known location:',
+                                      style: TextStyle(
+                                          color: Color.fromARGB(
+                                              255, 141, 147, 149)),
+                                    ),
+                                    Text(
+                                      character.location?.name ?? 'Unknown',
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    const Text(
+                                      'First seen in:',
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          color: Color.fromARGB(
+                                              255, 141, 147, 149)),
+                                    ),
+                                    Text(
+                                      character.episode?.isNotEmpty == true
+                                          ? character.episode![0].name ??
+                                              'Unknown'
+                                          : 'Unknown',
+                                      style: const TextStyle(
+                                          color: Colors.white, fontSize: 16),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ))
-                .toList(),
-          );
-        },
-        orElse: () {
-          return Container();
-        },
+                      ))
+                  .toList(),
+            );
+          },
+          orElse: () {
+            return Container();
+          },
+        ),
       ),
     );
   }
